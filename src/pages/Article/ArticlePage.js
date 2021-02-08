b97df9adf87a50aa47c1c390d181a008278d265e
hardikbajaj <hardik.bajaj.04@gmail.com>
2021-02-08 12:20:45 +0530

    responsive behavior

---
+++ src/pages/Article/ArticlePage.js
@@ -0,0 +1,12 @@
+import { Box } from '@material-ui/core'
+import React from 'react'
+
+export const ArticlePage = () => {
+  return (
+    <>
+      <Box className='container-column' style={{padding:'20px', alignItems:'flex-start'}}>
+        JOE BIDEN
+      </Box>
+    </>
+  )
+}
